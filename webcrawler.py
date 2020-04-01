3f88315e7e49c6afe5cd3a88738c4b2903bdc32b
glossywhite <szymonbaczyk1999@gmail.com>
2020-04-01 19:53:53 +0200

    parse urls to absolute urls

--- webcrawler.py
+++ webcrawler.py
@@ -7,6 +7,7 @@ Created on Wed Apr  1 19:34:14 2020
 
 import urllib.request
 from bs4 import BeautifulSoup 
+import urllib.parse
 
 print('Getting staff urls...')
 
@@ -26,7 +27,8 @@ urls = []
 
 for link in links:
     if len(link.get_text()) > 1:
-        url = link['href']
+        base_url = 'http://wa.amu.edu.pl'
+        url = urllib.parse.urljoin(base_url, link['href'])
         urls.append(url)
 
 print('Urls found:')
