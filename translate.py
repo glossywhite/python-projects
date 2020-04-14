a1656916a758e7f031270f6306a2a3a10a7cfbab
glossywhite <szymonbaczyk1999@gmail.com>
2020-04-14 19:52:06 +0200

    implemented translate library

--- translate.py
+++ translate.py
@@ -5,3 +5,11 @@ Created on Tue Apr 14 19:44:50 2020
 @author: szymo
 """
 
+from googletrans import Translator
+
+def main():
+    translator = Translator()
+    result = translator.translate('How do you do?',dest='pl')
+    print(result.text)
+    
+main()
