999279c81eb6a394375e321060a8b7f418482c23
glossywhite <szymonbaczyk1999@gmail.com>
2020-06-03 10:13:49 +0200

    added a way to extract elements with 'ol'

--- class21.py
+++ class21.py
@@ -10,3 +10,14 @@ from bs4 import BeautifulSoup
 
 argument = input('URL: ')
 response = urllib.request.urlopen(url)
+data = response.read()
+file = BeautifulSoup(data, 'html.parser')
+
+lists = file.find_all('ol')
+for li in lists:
+    i = 1
+    list_elements = li.find_all('li')
+    print('\n')
+    for l in list_elements:
+        print(str(i) + '. ' + l.get_text())
+        i += 1
